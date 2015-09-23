62539911dc6d87b879a53f4a8afdb05980902da4
helenetangen <helene.tangen@gmail.com>
2015-09-23 19:06:05 +0200

    Resolved AStar conflict

--- astar.py
+++ astar.py
@@ -2,7 +2,21 @@ __author__ = 'helenetangen'
 
 
 
+class Board:
 
 
+    def makeBoard(self, height, width):
+        board = [[0 for x in range(height)] for x in range(width)]
+        return board
 
 
+def main():
+    b = Board()
+    b.board = b.makeBoard(5, 5)
+    for i in range(5):
+        b.board[i][i] = 1
+        print b.board[i][i]
+
+
+if __name__ == '__main__':
+    main()
