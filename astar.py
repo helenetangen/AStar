abf178b7b1780c46482d930a15182340be41288e
helenetangen <helene.tangen@gmail.com>
2015-09-23 19:05:19 +0200

    Test

--- astar.py
+++ astar.py
@@ -1,15 +1,8 @@
 __author__ = 'helenetangen'
 
 
-print("Hello World!")
-print("Hellersiono Jean!")
-print("Hello Jean!")
-print("HELENE")
-print("Test")
 
 
-# A-star algorithm.
-def aStar():
 
 
 
