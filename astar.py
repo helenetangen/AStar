fecdc0dc6f860ef41840082954bc874a9a52ebf0
helenetangen <helene.tangen@gmail.com>
2015-09-25 12:49:54 +0200

    Small change

--- astar.py
+++ astar.py
@@ -18,23 +18,19 @@ class Node:
         self.h=h
         self.children = []
 
-<<<<<<< HEAD
-=======
+    "Blablabla"
 
->>>>>>> 39319835a0068abf7a0b390a3ce198a2d9efb34c
+
 def main():
     b = Board()
     b.board = b.makeBoard(5, 5)
     for i in range(5):
         b.board[i][i] = 1
-<<<<<<< HEAD
         print b.board[i][i]
-=======
        # print b.board[i][i]
 
     mynode = Node(None,0,0,0,0)
     print mynode.parent,mynode.children,mynode.Ch
->>>>>>> 39319835a0068abf7a0b390a3ce198a2d9efb34c
 
 
 if __name__ == '__main__':
