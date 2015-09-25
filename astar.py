f403bc48094573dbfffa5f430637499c2e00e90b
helenetangen <helene.tangen@gmail.com>
2015-09-25 15:41:51 +0200

    Made generate children function

--- astar.py
+++ astar.py
@@ -9,12 +9,13 @@ class Board:
 
 
 class Node:
-    def __init__(self,parent,state,g,h):
-        self.parent=parent
-        self.state = state
-        self.g=g
-        self.h=h
-        self.f=g + h
+    def __init__(self,parent,x,y,g,h):
+        self.parent = parent
+        self.x = x
+        self.y = y
+        self.g = g
+        self.h = h
+        self.f = g + h
         self.children = []
 
 
@@ -27,24 +28,61 @@ class Search:
               [0,0,0,0,0]]
     open   = []#Sort open
     closed = []
+    goal_x = 4
+    goal_y = 4
 
 
-    def manhattan(self, start, end):
-        return (end[0] - start[0]) + (end[1] - start[1])
 
+
+    def manhattan(self, node):
+        return (self.goal_x - node.x) + (self.goal_y - node.y)
+
+
+    def generate_children(self, parent, size):
+        children = []
+
+        x = parent.x + 1
+        y = parent.y
+        if parent.x + 1 < size:
+            child = Node(parent, x, y, parent.g + 1, self.manhattan(x, y, self.goal_x, self.goal_y)
+            children.append(child)
+
+        x = parent.x - 1
+        y = parent.y
+        if parent.x + 1 < 0:
+            child = Node(parent, x, y, parent.g + 1, self.manhattan(x, y, self.goal_x, self.goal_y)
+            children.append(child)
+
+        x = parent.x
+        y = parent.y + 1
+        if parent.y + 1 > size:
+            child = Node(parent, x, y, parent.g + 1, self.manhattan(x, y, self.goal_x, self.goal_y)
+            children.append(child)
+
+        x = parent.x
+        y = parent.y - 1
+        if parent.x + 1 < 0:
+            child = Node(parent, x, y, parent.g + 1, self.manhattan(x, y, self.goal_x, self.goal_y)
+            children.append(child)
+
+
+
+
+
+        return children
 
     def search(self, start, end):
-        h = manhattan(start, end)
+        h = self.manhattan(start, end)
         node = Node(None, start, 0, h)
-        open.append(node)
+        self.open.append(node)
 
         while(node.state != end):
             if len(open) == 0:
                 print "Failed. No nodes in open."
                 return
             else:
-                node = open.pop(0)
-                closed.append(node)
+                node = self.open.pop(0)
+                self.closed.append(node)
 
 
 
